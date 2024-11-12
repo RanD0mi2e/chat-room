e3548ff6e545d3c77eb0273cd5d8eafbacf6ba97
pangzijian <871119445@qq.com>
2024-11-12 18:23:30 +0800

    测试iconUrl能否正常使用

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -58,6 +58,7 @@ const ChannelMenuGroup = () => {
           {
             id: "profile",
             name: "个人主页",
+            iconSrc: "http://gips2.baidu.com/it/u=195724436,3554684702&fm=3028&app=3028&f=JPEG&fmt=auto?w=1280&h=960"
           },
         ];
         const menuGroupB: ChannelProps[] = [
