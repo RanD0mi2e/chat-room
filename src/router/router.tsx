66fa2ddda82448ade1259a0322eebbb399c98008
pangzijian <871119445@qq.com>
2024-11-29 14:22:00 +0800

    设置首页默认路由

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,14 @@
 import Dashboard from "../pages/Dashboard/Dashboard";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import ServerPage from "@/pages/Dashboard/ServerPage";
 import ProfilePage from "@/pages/ProfilePage/ProfilePage";
 import { Channel } from "@/pages/ChannelDetail/ChannelDetail";
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/channel/profile" replace />,
+  },
   {
     path: "/channel",
     element: <Dashboard />,
